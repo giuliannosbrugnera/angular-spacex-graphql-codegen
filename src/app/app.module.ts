ea003f62c72be1416acf786b3a5c0e7c89cc22cd
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2019-10-13 15:47:26 -0300

    Generate LaunchDetails component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LaunchListComponent } from './launch-list/launch-list.component';
+import { LaunchDetailsComponent } from './launch-details/launch-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LaunchListComponent
+    LaunchListComponent,
+    LaunchDetailsComponent
   ],
   imports: [
     BrowserModule,
